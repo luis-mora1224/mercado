d8552dc972ad42c4383d4e4a5420b00634815aed
luis-mora1224 <dark_100@live.com>
2021-07-15 20:56:18 -0500

    Correcion de visualizacion de los datos de la tabla (aun sin paginacion)

--- mercadoaplication/views/public/CategoriasView.py
+++ mercadoaplication/views/public/CategoriasView.py
@@ -55,7 +55,8 @@ def vw_categorias_Articulos_hogar(request):
 	if request.is_ajax and request.method == "POST":
 		try:
 			valor = request.POST['catetipo']
-			categorias = list(Categorias.objects.filter(catetipo = valor).order_by('idcategoria').values())#convertimos a un dicionario y a un arreglo
+			#categorias = list(Categorias.objects.filter(catetipo = valor).order_by('idcategoria').values())#convertimos a un dicionario y a un arreglo
+			categorias = list(Categorias.objects.all().order_by('idcategoria').values())
 			categoria = json.dumps(categorias)
 			#print(categoria)
 			return JsonResponse({'valido': categorias})
